b26e0063b4a6fd5da05b118eaf7871f43a82222b
Bohdan Pusanuk <pusanukbr@live.ru>
2017-12-13 15:25:35 +0200

    go home

--- app/js/common.js
+++ app/js/common.js
@@ -1,64 +1,72 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	/* When user clicks the Icon */
-	$('.submit a').click(function() {
-		$(".modal_window, .types_of_assistance").toggleClass('visible');
-	});
+    /* When user clicks the Icon */
+    $('.submit a').click(function() {
+        $(".modal_window, .types_of_assistance").toggleClass('visible');
+    });
 
-	$('.search i').click(function() {
-		$(".place, .place_resul").toggleClass('visible');
-	});
+    $('.search i').click(function() {
+        $(".place, .place_resul").toggleClass('visible');
+    });
 
-	$('.exit').click(function() {
-		$(".place, .place_resul").removeClass('visible');
-	});
+    $('.exit').click(function() {
+        $(".place, .place_resul").removeClass('visible');
+    });
 
-	$('.exit_modal').click(function() {
-		$(".modal_window, .types_of_assistance").removeClass('visible');
-	});
-	$(document).click(function() {
-		if ($(".modal_window").hasClass("visible")) {
-			$(document).mouseup(function(e) { 
-				var div = $('.modal_window'); 
-				if (!div.is(e.target)
-					&&
-					div.has(e.target).length === 0) { 
-					$(".modal_window, .types_of_assistance").removeClass('visible');
-				}
-			});
-		} else if ($(".place").hasClass("visible") && $(".place_resul").hasClass("visible")) {
-			$(document).mouseup(function(e) {
-				var div = $('.place, .place_resul');
-				if (!div.is(e.target)
-					&&
-					div.has(e.target).length === 0) { 
-					$(".place, .place_resul").removeClass('visible');
-				}
-			});
-		}
-	});
+    $('.exit_modal').click(function() {
+        $(".modal_window, .types_of_assistance").removeClass('visible');
+    });
+    $(document).click(function() {
+        if ($(".modal_window").hasClass("visible")) {
+            $(document).mouseup(function(e) {
+                var div = $('.modal_window');
+                if (!div.is(e.target) &&
+                    div.has(e.target).length === 0) {
+                    $(".modal_window, .types_of_assistance").removeClass('visible');
+                }
+            });
+        } else if ($(".place").hasClass("visible") && $(".place_resul").hasClass("visible")) {
+            $(document).mouseup(function(e) {
+                var div = $('.place, .place_resul');
+                if (!div.is(e.target) &&
+                    div.has(e.target).length === 0) {
+                    $(".place, .place_resul").removeClass('visible');
+                }
+            });
+        }
+    });
 
 
-	 $('.slide-for').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		fade: true,
-		asNavFor: '.slider-nav',
-		infinite: true,
-		dots: false,
-	});
-	$('.slider-nav').slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		asNavFor: '.slide-for',
-		centerMode: true,
-		focusOnSelect: true,
-		dots: false,
-		centerPadding: '100px',
-		arrows: false,
-		infinite: false,
-	});
+    $('.slide-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.slider-nav',
+        infinite: true,
+        dots: false,
+        fade: true
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slide-for',
+        centerMode: true,
+        focusOnSelect: true,
+        dots: false,
+        centerPadding: '100px',
+        arrows: false,
+        infinite: false,
+        // fade: true
+    });
+
+    $('.list').click(function(e) {
+        $('.list').removeClass('active');
+        $(this).toggleClass('active');
+        e.preventDefault();
+    });
+
+
+
 
 
 });
